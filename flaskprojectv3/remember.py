076c695d6d275b6b01634f2dd7562d080cfe1928
kaybeard <kmbrickel@fullsail.edu>
2017-04-30 16:31:48 -0400

    Updating features
    Updating new features CRUD OPs, about, etc

--- flaskprojectv3/remember.py
+++ flaskprojectv3/remember.py
@@ -145,6 +145,26 @@ def add_message():
         flash('Your message was recorded')
     return redirect(url_for('timeline'))
 
+# Deleting a message Post from Reminder
+@app.route('/delete_message', methods=['POST'])
+def delete_message():
+    if 'user_id' not in session:
+        abort(401)
+
+    #if request.form['text'] or request.form['cat']:
+        db = get_db()
+        db.execute('''delete from message WHERE author_id = ?''', (session['user_id'], request.form['text'], request.form['message_id'],
+                                                                   int(time.time())))
+        db.commit()
+        flash('Message was deleted')
+    return redirect(url_for('timeline'))
+
+
+#About
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 # Login
 @app.route('/login', methods=['GET', 'POST'])
 def login():
